4f4decd0fad998bc15a10384114c2d42e9877681
ZC <>
2023-12-23 13:32:49 +0800

    Our current conundrum

--- Build a meme generator/Our current conundrum/src/App.jsx
+++ Build a meme generator/Our current conundrum/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [things, setThings] = React.useState(["Thing 1", "Thing 2"]);
+
+  // const thingsArray = ["Thing 1", "Thing 2"];
+
+  function addItem() {
+    const newThingText = `Thing ${things.length + 1}`;
+    setThings((prevState) => [...prevState, newThingText]);
+    // thingsArray.push(newThingText);
+    // console.log(thingsArray);
+  }
+
+  const thingsElements = things.map((thing) => <p key={thing}>{thing}</p>);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      {thingsElements}
+    </div>
+  );
 }
 
-export default App
+export default App;
