cf35bbdb60cae993c548dc68aae6e29559c56f89
ZC <>
2023-12-27 10:57:48 +0800

    isGoingOut

--- Build a meme generator/useState/src/App.jsx
+++ Build a meme generator/useState/src/App.jsx
@@ -1,38 +1,23 @@
 import React from "react";
 
 function App() {
-  const [count, setCount] = React.useState(0);
-
   /**
-   * Note: if you ever need the old value of state
-   * to help you determine the new value of state,
-   * you should pass a callback function to your
-   * state setter function instead of using
-   * state directly. This callback function will
-   * receive the old value of state as its parameter,
-   * which you can then use to determine your new
-   * value of state.
+   * Challenge:
+   * - Initialize state for `isGoingOut` as a boolean
+   * - Make it so clicking the div.state--value flips that
+   *   boolean value (true -> false, false -> true)
+   * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
    */
-
-  function add() {
-    setCount((prevCount) => prevCount + 1);
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
+  function changeMind() {
+    setIsGoingOut((prevState) => !prevState);
   }
 
-  // Challenge: update `substract` to use a callback function
-
-  function subtract() {
-    setCount((prevCount) => prevCount - 1);
-  }
   return (
-    <div className='counter'>
-      <button className='counter--minus' onClick={subtract}>
-        –
-      </button>
-      <div className='counter--count'>
-        <h1>{count}</h1>
-      </div>
-      <button className='counter--plus' onClick={add}>
-        +
+    <div className='state'>
+      <h1 className='state--title'>Do I feel like going out tonight?</h1>
+      <button className='state--value' onClick={changeMind}>
+        {isGoingOut ? "Yes" : "No"}
       </button>
     </div>
   );
